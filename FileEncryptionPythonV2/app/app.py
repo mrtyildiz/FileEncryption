5046fda8d1fb3fc9581782f97f4f5c1abcefbc29
mrtyildiz <muratasnb@hotmail.com>
2023-07-10 17:39:13 +0300

    Create app.py

---
+++ FileEncryptionPythonV2/app/app.py
@@ -0,0 +1,157 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+
+from fastapi import File, UploadFile
+import shutil
+import base64
+from fastapi.responses import FileResponse
+from Create_AES import AES_Create
+from Create_RSA import RSACreate
+from FileEncryptPYHSM import encrypt_file, decrypt_file
+from FileEncryptRSA import encrypt_file_rsa, decrypt_file_rsa
+from FileEncryptRSApkcs11 import rsa_encrypt_file, rsa_decrypt_file
+import os
+app = FastAPI()
+
+class AESKey(BaseModel):
+    ID: int
+    PIN: str
+    KName: str
+class FileUploadEnc(BaseModel):
+    ID: int
+    PIN: str
+    init_vector: str
+    KName: str
+    FNamePath: str
+
+class FileUploadDec(BaseModel):
+    ID: int
+    PIN: str
+    KName: str
+    init_vector: bytes
+    ENamePath: str
+
+class RSAKey(BaseModel):
+    ID: int
+    PIN: str
+    PublicLabel: str
+    PrivateLabel: str
+
+class FileEncryptRSA(BaseModel):
+    ID: int
+    PIN: str
+    FileName: str
+    PublicKeyName: str
+
+class FileDecryptRSA(BaseModel):
+    ID: int
+    PIN: str
+    FileEncName: str
+    PrivateKeyName: str
+
+class Encryptpkcs11(BaseModel):
+    ID: int
+    PIN: str
+    FileName: str
+    PublicKeyName: str
+
+class Decryptpkcs11(BaseModel):
+    ID: int
+    PIN: str
+    FileEncName: str
+    PrivateKeyName: str
+
+@app.post("/AESCreate/")
+def AESCreate(data: AESKey):
+    # Gelen verileri kullanarak kaydetme işlemini gerçekleştirin
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    KeyName = data.KName
+    handler = AES_Create(Slot_ID,Slot_PIN,KeyName)
+    return {"Oluşturulan Anahtarın handler değeri": handler}
+
+@app.post("/RSACreate/")
+def RSA_Create(data: RSAKey):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    PublicLabel = data.PublicLabel
+    PrivateLabel = data.PrivateLabel
+    result = RSACreate(Slot_ID,Slot_PIN,PublicLabel,PrivateLabel)
+    return {result}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.environ.get("File_Path")
+    file_destination = f"{file_path}/{file.filename}"
+    with open(file_destination, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    print("Yüklenen dosyanın adı:", file.filename)
+    print("/app/"+file.filename)
+    return {"message": "Dosya başarıyla yüklendi", "filename": file.filename}
+
+@app.post("/download")
+def download_file(filename: str = Form(...)):
+    path = file_path = os.environ.get("File_Path")
+    file_path = str(path)+"/"+str(filename)
+    return FileResponse(file_path, filename=filename)
+@app.post("/FileEncPYHSM")
+def FileEncryption(data: FileUploadEnc):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    Init_Vector = data.init_vector
+    Init_Vector_bytes = base64.b64decode(Init_Vector)
+    KName = data.KName
+    FNamePath = data.FNamePath
+    result = encrypt_file(Slot_ID, Slot_PIN, FNamePath, KName, Init_Vector_bytes)
+    return result
+
+@app.post("/FileDecPYHSM")
+def FileDecryption(data: FileUploadDec):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    Init_Vector = data.init_vector
+    Init_Vector_bytes = base64.b64decode(Init_Vector)
+    KName = data.KName
+    FNamePath = data.ENamePath
+    result = decrypt_file(Slot_ID, Slot_PIN, FNamePath, KName, Init_Vector_bytes)
+    return result
+
+
+@app.post("/RSAFileEncrypt")
+def RSA_File_Enc_Value(data: FileEncryptRSA):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    File_Name = data.FileName
+    PubName = data.PublicKeyName
+    result = encrypt_file_rsa(Slot_ID, File_Name, PubName,Slot_PIN)
+    return result
+
+@app.post("/RSAFileDecrypt")
+def RSA_File_Dec_Value(data: FileDecryptRSA):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    File_Name = data.FileEncName
+    PrivName = data.PrivateKeyName
+    result = decrypt_file_rsa(Slot_ID, File_Name, PrivName, Slot_PIN)
+    return result
+
+@app.post("/RSAFileEnc")
+def RSA_Enc(data: Encryptpkcs11):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    File_Name = data.FileName
+    PubName = data.PublicKeyName
+    result = rsa_encrypt_file(Slot_ID, File_Name, PubName)
+    return result
+
+@app.post("/RSAFileDec")
+def RSA_Dec(data: Decryptpkcs11):
+    Slot_ID = data.ID
+    Slot_PIN = data.PIN
+    File_Name = data.FileEncName
+    Priv_Name = data.PrivateKeyName
+    result = rsa_decrypt_file(Slot_ID, Slot_PIN, File_Name, Priv_Name)
+    return result
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
